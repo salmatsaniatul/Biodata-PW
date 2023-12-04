ca0cecb30a87b3fc3d6a4c9ded106f9896f35a3b
salmatsaniatul <141211517+salmatsaniatul@users.noreply.github.com>
2023-12-04 13:34:49 +0700

    Add files via upload

---
+++ BIODATA/identitas.js
@@ -0,0 +1,23 @@
+const NIS = 22310242;
+const noAbsen = 27;
+let nama = "Salma Tsaniatul M";
+let kelas = "XI-PPLG 2";
+let jurusan = "RPL";
+let hobi = "Berenang";
+let favDrink = "Matcha Latte";
+let favFood = "Chicken";
+let alamat = "Kp.Simpangsari Kec.Purbaratu Kel.Sukajaya Kota Tasikmalaya Jawa Barat";
+let citacita = "Guru Aamiin"
+
+document.write(
+  "NIS :" + NIS,
+  "<br>Nomor Absen :" + noAbsen,
+  "<br>Nama :" + nama,
+  "<br>Kelas :" + kelas,
+  "<br>Jurusan :" +jurusan,
+  "<br>Hobi :" + hobi,
+  "<br>Minuman Favorit :" + favDrink,
+  "<br>Makanan Favorit :" + favFood,
+  "<br>alamat :" + alamat,
+  "<br>citaCita :" + citacita
+);
